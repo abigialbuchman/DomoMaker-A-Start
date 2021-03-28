5a64a66a05f0f89a5f881ed01603c3cf44a06822
Abigail Buchman <amb5759@rit.edu>
2021-03-28 18:19:55 -0400

    fixed a typo, still not running. a file in the node modules throwing routing error.

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
-//const session = require('express-session');
+// const session = require('express-session');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -21,7 +21,7 @@ const mongooseOptions = {
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
   if (err) {
-    console.log('Could not connect tot database');
+    console.log('Could not connect to database');
     throw err;
   }
 });
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
-app.set('view engine', 'hanldebars');
+app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
 
